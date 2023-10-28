8b3f583738cd0901b742621c7020d00d495dee73
katoys <yasuaki-kato@bm-sms.co.jp>
2023-10-28 12:58:48 +0900

    test Version

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -19,6 +19,8 @@ repositories {
 }
 
 dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+
     // Use the Kotlin JUnit 5 integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
---
+++ plugin/src/test/kotlin/io/github/katoys/version/incrementer/SemanticVersionTest.kt
@@ -0,0 +1,146 @@
+package io.github.katoys.version.incrementer
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class SemanticVersionTest {
+
+    @Nested
+    inner class From {
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "0.0.0,0,0,0,",
+                "1.2.3,1,2,3,",
+                "999.999.999,999,999,999,",
+                "1.2.3-SNAPSHOT,1,2,3,SNAPSHOT"
+            ]
+        )
+        fun `from string`(value: String, major: Int, minor: Int, patch: Int, suffix: String?) {
+            // when
+            val actual = SemanticVersion.from(value)
+            // then
+            assertEquals(value, actual.value)
+            assertEquals(major, actual.element.major)
+            assertEquals(minor, actual.element.minor)
+            assertEquals(patch, actual.element.patch)
+            assertEquals(suffix, actual.element.suffix)
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "0.0.0,0,0,0,",
+                "1.2.3,1,2,3,",
+                "999.999.999,999,999,999,",
+                "1.2.3-SNAPSHOT,1,2,3,SNAPSHOT"
+            ]
+        )
+        fun `from element`(value: String, major: Int, minor: Int, patch: Int, suffix: String?) {
+            // when
+            val actual = SemanticVersion.from(
+                major = major,
+                minor = minor,
+                patch = patch,
+                suffix = suffix
+            )
+            // then
+            assertEquals(value, actual.value)
+            assertEquals(major, actual.element.major)
+            assertEquals(minor, actual.element.minor)
+            assertEquals(patch, actual.element.patch)
+            assertEquals(suffix, actual.element.suffix)
+        }
+    }
+
+    @Nested
+    inner class Up {
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "0.0.0,1.0.0",
+                "1.2.3,2.0.0",
+                "999.999.999,1000.0.0",
+                "1.2.3-SNAPSHOT,2.0.0-SNAPSHOT"
+            ]
+        )
+        fun `up major`(value: String, expected: String) {
+            // when
+            val actual = SemanticVersion.from(value).upMajor()
+            // then
+            assertEquals(expected, actual.value)
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "0.0.0,0.1.0",
+                "1.2.3,1.3.0",
+                "999.999.999,999.1000.0",
+                "1.2.3-SNAPSHOT,1.3.0-SNAPSHOT"
+            ]
+        )
+        fun `up minor`(value: String, expected: String) {
+            // when
+            val actual = SemanticVersion.from(value).upMinor()
+            // then
+            assertEquals(expected, actual.value)
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "0.0.0,0.0.1",
+                "1.2.3,1.2.4",
+                "999.999.999,999.999.1000",
+                "1.2.3-SNAPSHOT,1.2.4-SNAPSHOT"
+            ]
+        )
+        fun `up patch`(value: String, expected: String) {
+            // when
+            val actual = SemanticVersion.from(value).upPatch()
+            // then
+            assertEquals(expected, actual.value)
+        }
+    }
+
+    @Nested
+    inner class Suffix {
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "1.2.3,SNAPSHOT,1.2.3-SNAPSHOT",
+                "1.2.3-SNAPSHOT,SNAPSHOT,1.2.3-SNAPSHOT",
+                "1.2.3-SNAPSHOT,RELEASE,1.2.3-RELEASE",
+                "1.2.3-SNAPSHOT,,1.2.3",
+                "1.2.3-SNAPSHOT,'',1.2.3",
+            ]
+        )
+        fun `append suffix`(value: String, suffix: String?, expected: String) {
+            // when
+            val actual = SemanticVersion.from(value).appendSuffix(suffix)
+            // then
+            assertEquals(expected, actual.value)
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "1.2.3-SNAPSHOT,1.2.3",
+                "1.2.3-RELEASE,1.2.3",
+                "1.2.3,1.2.3",
+            ]
+        )
+        fun `remove suffix`(value: String, expected: String) {
+            // when
+            val actual = SemanticVersion.from(value).removeSuffix()
+            // then
+            assertEquals(expected, actual.value)
+        }
+    }
+}
