eba3db56c2802b0a99f11fc352887287a4efee56
katoys <yasuaki-kato@bm-sms.co.jp>
2024-07-11 14:36:55 +0900

    kotlin plugin vup to 2.0.0

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-gradle-plugin`
-    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
     id("com.gradle.plugin-publish") version "1.2.1"
 }
 
