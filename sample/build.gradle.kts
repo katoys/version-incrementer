6c8e441fe9cf0eaafdabf16b72d45ba70ba7c51a
katoys <yasuaki-kato@bm-sms.co.jp>
2024-07-14 14:37:56 +0900

    apply v1.2.0

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,7 +1,7 @@
 import io.github.katoys.version.incrementer.semantic.SemanticVersioning
 
 plugins {
-    id("io.github.katoys.version-incrementer") version "1.0.0"
+    id("io.github.katoys.version-incrementer") version "1.2.0"
 }
 
 group = "io.github.katoys"
